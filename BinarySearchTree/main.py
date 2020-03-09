be686ea972c8d93561151e24ed09755e20d630d6
AndyJ <andrewdaviesul@gmail.com>
2020-03-09 17:23:19 +0000

    formatting output

--- BinarySearchTree/main.py
+++ BinarySearchTree/main.py
@@ -1,26 +1,27 @@
-# Binary Search Driver Program
+# Data Structures -  Binary Search Tree
 
-from BinarySearchTree.bst import BST
+from bst import BST
 import random
 
 
-def main():
-    """ test binary search tree methods """
+# Program driver
+if __name__ == '__main__':
 
-    lower, upper, n_val = 1, 1000, 300
-    find_val = random.randint(lower, upper)
+    # number of values between upper and lower limits
+    lower, upper, n_val = 1, 1000, 100
 
+    # value used to test search method in bst
+    search_val = random.randint(lower, upper)
+
+    # generate array of non-repeating random values
     values = random.sample(range(lower, upper), n_val)
+
     print(values, end='\n\n')
 
-    bst = BST()
+    bst = BST()  # instantiate binary search tree object
     for v in values:
-        bst.add_leaf(v)
+        bst.add_leaf(v)  # add leafs to tree
 
-    bst.print_order()
-    print('\nheight of binary tree: ', bst.height())
-    print('found value: {} : {}'.format(find_val, bst.search(find_val)))
-
-
-if __name__ == '__main__':
-    main()
+    bst.print_order()  # print all values in order
+    print('\n\nheight of binary tree: ', bst.height())
+    print('\nfound: {} : {}\n'.format(search_val, bst.search(search_val)))
